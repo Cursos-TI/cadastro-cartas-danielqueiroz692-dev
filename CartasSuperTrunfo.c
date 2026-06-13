ce950190a91d7ee8207e1f2bf14abfca4e4a7fa1
danielqueiroz692-dev <danielqueiroz692@gmail.com>
2026-06-13 18:10:28 +0000

    Primeira verão do código, trabalhando com váriaveis e funções simples

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,65 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char state_1[3], state_2[3];
+  char code_card_1[20], code_card_2[20];
+  char city_1[20], city_2[20];
+  int population_1, population_2;
+  float area_1, area_2;
+  float pib_1, pib_2;
+  int tourist_points_1, tourist_points_2;
   // Área para entrada de dados
+  printf("Primeira Carta\n");
+  printf("Informe o estado em que a cidade fica localizada\n");
+  scanf("%s", state_1);
+
+  printf("Informe o código da carta seguindo o padrão: Estado + 01, 02, etc EX: RN01\n");
+  scanf("%s", code_card_1);
+
+  printf("Informe o nome da cidade\n");
+  scanf("%s", city_1);
+
+  printf("Informe a concentração populacional da cidade\n");
+  scanf("%d", &population_1);
+
+  printf("Informe a tamanho da cidade em Km²\n");
+  scanf("%f", &area_1);
+
+  printf("Informe o PIB da cidade\n");
+  scanf("%f", &pib_1);
+
+  printf("Informe a quuantidade de pontos túristicos\n");
+  scanf("%d", &tourist_points_1);
+
+  printf("Segunda Carta\n");
+  printf("Informe o estado em que a cidade fica localizada\n");
+  scanf("%s", state_2);
+
+  printf("Informe o código da carta seguindo o padrão: Estado + 01, 02, etc EX: RN01\n");
+  scanf("%s", code_card_2);
+
+  printf("Informe o nome da cidade\n");
+  scanf("%s", city_2);
+
+  printf("Informe a concentração populacional da cidade\n");
+  scanf("%d", &population_2);
+
+  printf("Informe a tamanho da cidade em Km²\n");
+  scanf("%f", &area_2);
+
+  printf("Informe o PIB da cidade\n");
+  scanf("%f", &pib_2);
+
+  printf("Informe a quantidade de pontos túristicos\n");
+  scanf("%d", &tourist_points_2);
 
   // Área para exibição dos dados da cidade
+
+  printf("PRIMEIRA CARTA| \n Estado: %s \n Código da carta: %s \n Cidade: %s \n", state_1, code_card_1, city_1);
+  printf(" População: %d \n Área em Km²: %.2f \n PIB: %.2f \n Pontos túristicos %d \n", population_1, area_1, pib_1, tourist_points_1);
+
+  printf("SEGUNDA CARTA| \n Estado: %s \n Código da carta: %s \n Cidade: %s \n", state_2, code_card_2, city_2);
+  printf(" População: %d \n Área em Km²: %.2f \n PIB: %.2f \n Pontos túristicos %d", population_2, area_2, pib_2, tourist_points_2);
 
 return 0;
 } 

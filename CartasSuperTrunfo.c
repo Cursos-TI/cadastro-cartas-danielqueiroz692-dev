a721728b6793f1a34788e67e585ecde0c072cdbb
danielqueiroz692-dev <danielqueiroz692@gmail.com>
2026-07-01 17:45:54 +0000

    Trabalhando com menu interativo usando switch e aplicando logíca com if e else

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,11 +17,12 @@ int main() {
   int tourist_points_1, tourist_points_2;
   double population_density_1, population_density_2;
   double pib_capita_1, pib_capita_2;
+  unsigned choice;
 
   // Área para entrada de dados
 
   printf("Primeira Carta\n");
-  printf("Informe o estado em que a cidade fica localizada\n");
+  printf("Informe o estado em que a cidade fica localizada, escreva a sigla correspondente ao estado. ex: RN, RS, AM, etc\n");
   scanf("%s", state_1);
 
   printf("Informe o código da carta seguindo as regras, Sigla do estado mais númeração 01, 02, etc. EX: RN01\n");
@@ -70,48 +71,99 @@ int main() {
   population_density_2 = (population_2 / area_2);
   pib_capita_2 = (pib_2 / population_2);
   
-  double super_power_1 = (double) (population_1 + area_1 + pib_1 + tourist_points_1 + pib_capita_1 + population_density_1);
-  double super_power_2 = (double) (population_2 + area_2 + pib_2 + tourist_points_2 + pib_capita_2 + population_density_2);
-
   // Área para exibição dos dados da cidade
-  printf("\n==================================================\n");
-  printf("Resultado do jogo \n");
-  if(population_1 > population_2){
-    printf("ATRIBUTO POPULAÇÃO: CARTA %s ganhou \n", city_1);
-  } else{
-    printf("ATRIBUTO POPULAÇÃO: CARTA %s ganhou \n", city_2);
+  printf("Escolha qual dos atributos você deseja fazer a comparação\n"
+        "1: População\n"
+        "2: Área\n"
+        "3: PIB\n"
+        "4: Quantidade de pontos turisticos\n"
+        "5: Densidade populacional\n"
+        "6: PIB per Capita\n");
+  scanf("%u", &choice); 
+  unsigned result = 0;
+
+  switch (choice) {
+  case 1:
+    if (population_1 > population_2) {
+      result = 1;
+    } else if (population_1 < population_2) {
+      result = 2;
+    } else {
+      result = 0;
+    }
+    break;
+  case 2:
+    if (area_1 > area_2) {
+      result = 1;
+    } else if (area_1 < area_2) {
+      result = 2;
+    } else {
+      result = 0;
+    }
+    break;
+  case 3:
+    if (pib_1 > pib_2) {
+      result = 1;
+    } else if (pib_1 < pib_2) {
+      result = 2;
+    } else {
+      result = 0;
+    }
+    break;
+  case 4:
+    if (tourist_points_1 > tourist_points_2) {
+      result = 1;
+    } else if (tourist_points_1 < tourist_points_2) {
+      result = 2;
+    } else {
+      result = 0;
+    }
+    break;
+  case 5:
+    if (population_density_1 > population_density_2) {
+      result = 2;
+    } else if (population_density_1 < population_density_2) {
+      result = 1;
+    } else {
+      result = 0;
+    }
+    break;
+  case 6:
+    if (pib_capita_1 > pib_capita_2) {
+      result = 1;
+    } else if (pib_capita_1 < pib_capita_2) {
+      result = 2;
+    } else {
+      result = 0;
+    }
+    break;
+  default:
+    printf("ERROR! valor inválido");
+    return 1;
   }
 
-  if(area_1 < area_2){
-    printf("ATRIBUTO ÁREA: CARTA %s ganhou \n", city_1);
-  } else {
-    printf("ATRIBUTO ÁREA: CARTA %s ganhou \n", city_2);
-  }
+if (result == 1) {
+  printf("### Carta 1 ganhou ### \n Cidade 1: %s \n Cidade 2: %s\n", city_1, city_2);
+} else if (result == 2) {
+  printf("### Carta 2 ganhou ### \n Cidade 1: %s \n Cidade 2: %s\n", city_1, city_2);
+} else {
+  printf("Empate !! \n Cidade 1: %s \n Cidade 2: %s\n", city_1, city_2);
+}
+printf("Atributo comparado: ");
 
-  if(pib_1 > pib_2){
-    printf("ATRIBUTO PIB: CARTA %s ganhou \n", city_1);
-  } else{
-    printf("ATRIBUTO PIB: CARTA %s ganhou \n", city_2);
-  }
-
-  if(tourist_points_1 > tourist_points_2){
-    printf("ATRIBUTO TURISMO: CARTA %s ganhou \n", city_1);
-  } else{
-    printf("ATRIBUTO TURISMO: CARTA %s ganhou \n", city_2);
-  }
-
-  if(population_density_1 < population_density_2){
-    printf("ATRIBUTO DENSIDADE POPULACIONAL: CARTA %s ganhou \n", city_1);
-  } else{
-    printf("ATRIBUTO DENSIDADE POPULACIONAL: CARTA %s ganhou \n", city_2);
-  }
-
-  if(pib_capita_1 > pib_capita_2) {
-    printf("ATRIBUTO PIB PER CAPITA: CARTA %s ganhou \n", city_1);
-  } else{
-    printf("ATRIBUTO PIB PER CAPITA: CARTA %s ganhou \n", city_2);
-  }
-  printf("==================================================\n");
+if (choice == 1) {
+    printf("População\n Valores dos Atributos:\n Cidade 1: %lu\n Cidade 2: %lu\n", population_1, population_2);
+} else if (choice == 2) {
+    printf("Área\n Valores dos Atributos:\n Cidade 1: %.2f\n Cidade 2: %.2f\n", area_1, area_2);
+} else if (choice == 3) {
+    printf("PIB\n Valores dos Atributos:\n Cidade 1: %.2f\n Cidade 2: %.2f\n", pib_1, pib_2);
+} else if (choice == 4) {
+    printf("Quantidade de pontos turisticos\n Valores dos Atributos:\n Cidade 1: %d\n Cidade 2: %d\n", tourist_points_1, tourist_points_2);
+} else if (choice == 5) {
+    printf("Densidade populacional\n Valores dos Atributos:\n Cidade 1: %.2f\n Cidade 2: %.2f\n", population_density_1, population_density_2);
+} else if (choice == 6) {
+    printf("PIB per capita\n Valores dos Atributos:\n Cidade 1: %.2f\n Cidade 2: %.2f\n", pib_capita_1, pib_capita_2);
+}
 
 return 0;
-} 
+}
